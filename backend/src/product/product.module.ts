81361990171a9a638f715183bf77de785a97a65c
maksym <maksym.bachynskyi@aaa-gth.com>
2024-05-02 11:58:06 +0200

    fix error

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -1,6 +1,7 @@
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
+import { CurrencyModule } from '@/currency/currency.module';
 import { ProductController } from './product.controller';
 import { Product } from './product.entity';
 import { Price } from './price/price.entity';
@@ -9,11 +10,14 @@ import { Currency } from '@/currency/currency.entity';
 import { ProductStorefrontController } from './product.storefront.controller';
 import { ProductStorefrontService } from './product.storefront.service';
 import { ImageModule } from '@/image/image.module';
+import { SiteModule } from '@/site/site.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, Price, Site, Currency]),
+    CurrencyModule,
     ImageModule,
+    SiteModule,
   ],
   controllers: [ProductController, ProductStorefrontController],
   providers: [ProductService, ProductStorefrontService],
